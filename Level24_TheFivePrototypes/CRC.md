22a40a9cc693a8aab64371495a765c52657da8b6
rdthree <1165540+rdthree@users.noreply.github.com>
2022-03-07 21:14:07 -0500

    Changed names to make them more descriptive
    Working through the various challenges in Level 24

---
+++ Level24_TheFivePrototypes/CRC.md
@@ -0,0 +1,109 @@
+﻿***CRC Card***
+
+| Class            |              |
+|------------------|--------------|
+| Responsibilities | Collaborator |
+
+# The Point
+
+Define a point class with properties for X and Y. Each point has an x coordinate, a side to side distance from an
+origin, a y coordinate
+
+## Objectives & CRC
+
+- Define a new Point class with properties for X and Y
+- Add a constructor to create a point from specific x and y coordinates
+- Add a parameterless constructor to create a point at origin (0,0)
+- Display a point at (2,3) and (-4,0) as a test
+- Are X and Y immutable? Why? Why not?
+
+| Point Class   |       |
+|---------------|-------|
+| X position    | Point |
+| Y Position    |       |
+| Origin (0,0)  |       |
+| Display (x,y) |       |
+
+---
+
+# The Color
+
+Color class representing a color. Colors consists of three channels R, G, B. Each channel can be from 0 to 255. 0 is
+completely off, 255 is completely on. Examples of commonly used colors:
+
+- White (255, 255, 255)
+- Black (0, 0, 0)
+- Red (255, 0, 0)
+- Orange (255, 165, 0)
+- Yellow (255, 255, 0)
+- Green (0, 128, 0)
+- Blue (0, 0, 255)
+- Purple (128, 0, 128)
+
+## Objectives & CRC
+
+- define a new Color class with properties for R, G, B channels
+- constructors for new color objects
+- static properties for 8 commonly used colors
+- two test variables, display the RGB channels of a custom color and a static color
+
+| Color Class       |       |
+|-------------------|-------|
+| R channel         | Color |
+| G channel         |       |
+| B channel         |       |
+| 8 common colors   |       |
+| Display (R, G, B) |       |
+
+---
+
+# The Card
+
+Playing cards with a color (red, green, blue, yellow) and a rank (1 through 10, followed by symbols $,%,^,&) Symbol
+cards are equivalent to face cards.
+
+## Objectives & CRC
+
+- enums for card colors and ranks
+- Card class
+- tell if a card is a number or symbol card
+- method to create a card instance for the whole deck (every number, every rank) and display each with a string name
+- example: 14 red cards {1 thru 10 + (4) ranks)
+
+| Card Class                            |      |
+|---------------------------------------|------|
+| Red, Green, Blue, Yellow cards        | Card |
+| 1 thru 10 cards (in a color)          |      |
+| $, %, ^, & cards (in a color)         |      |
+| identify card: number or symbol       |      |
+| deck: all colors (numbers or symbols) |      |
+
+---
+
+# The Locked Door
+
+A door that can only lock with the correct passcode. Operation:
+
+- Open door can always close
+- Closed Unlocked can always open
+- Closed Unlocked can always lock
+- Closed Locked needs a password to unlock
+- Door gets passcode upon creation
+- Passcode can be changed by using the current code
+
+## Objectives & CRC
+
+- Door class tracks locked, open, closed
+- Performs transitions with methods
+- User provided initial passcode ad door creation
+- Method to change passcode
+
+| Door Class                              |      |
+|-----------------------------------------|------|
+| Open -> Close -> Lock -> Unlock -> Open | Door |
+| Password at Door Creation               |      |
+| Password to Unlock                      |      |
+| Change Password using old Password      |      |
+| User can operate door continuously      |      |
+
+---
---
+++ Level24_TheFivePrototypes/Program.cs
@@ -0,0 +1,183 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// point tests
+
+using Microsoft.VisualBasic;
+
+var point1 = new Point(2.0, 3.0);
+var point2 = new Point(-4.0, 0.0);
+var origin = Point.Origin();
+Console.WriteLine($"x = {point1.X}, y = {point1.Y}");
+Console.WriteLine($"origin = ({origin.X},{origin.Y})");
+
+// color tests
+var color1 = new Color(300, -3, 20);
+var color2 = new Color(50, 30, 40);
+var color3 = Color.Purple;
+Console.WriteLine($"color1: R{color1.R}, G{color1.G}, B{color1.B}");
+Console.WriteLine($"color2: R{color2.R}, G{color2.G}, B{color2.B}");
+Console.WriteLine($"color3: R{color3.R}, G{color3.G}, B{color3.B}");
+
+// card tests
+List<Card> deck = new List<Card>();
+var cardNumber = 0;
+foreach (var color in Enum.GetValues<CardColor>())
+{
+    foreach (var rank in Enum.GetValues<CardRank>())
+    {
+        // this is the only way the enum works
+        deck.Add(new Card
+        {
+            Color = color,
+            Rank = rank
+        });
+    }
+}
+
+foreach (var card in deck)
+{
+    Console.Write($"{card.Color}, {card.Rank}, {(int)card.Rank}");
+    if (card.RankChecker()) Console.Write(": is a face card");
+    Console.WriteLine("");
+}
+
+
+/// <summary>
+/// Point Class with X and Y coordinates
+/// - includes static method for Origin
+/// </summary>
+internal class Point
+{
+    /// <summary>
+    /// X coordinate
+    /// </summary>
+    public double X { get; }
+
+    /// <summary>
+    /// Y coordinate
+    /// </summary>
+    public double Y { get; }
+
+    /// <summary>
+    /// Point Constructor
+    /// </summary>
+    /// <param name="x">X coordinate</param>
+    /// <param name="y">Y coordinate</param>
+    internal Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    /// <summary>
+    /// Point at Origin
+    /// </summary>
+    /// <returns>Point at x,y = 0,0</returns>
+    public static Point Origin() => new Point(0.0, 0.0);
+}
+
+/// <summary>
+/// Color class with R, G and B channels.  Each 0 to 255.  Includes 8 built in colors.
+/// </summary>
+internal class Color
+{
+    public int R { get; }
+    public int G { get; }
+    public int B { get; }
+
+    /// <summary>
+    /// Color Constructor
+    /// </summary>
+    /// <param name="r">Red Channel 0 to 255</param>
+    /// <param name="g">Green Channel 0 to 255</param>
+    /// <param name="b">Blue Channel 0 to 255</param>
+    internal Color(int r, int g, int b)
+    {
+        R = twoFiveFive(r);
+        G = twoFiveFive(g);
+        B = twoFiveFive(b);
+    }
+
+    public static Color White => new Color(255, 255, 0);
+    public static Color Black => new Color(0, 0, 0);
+    public static Color Red => new Color(255, 0, 0);
+    public static Color Orange => new Color(255, 165, 0);
+    public static Color Yellow => new Color(255, 255, 0);
+    public static Color Green => new Color(0, 128, 0);
+    public static Color Blue => new Color(0, 0, 255);
+    public static Color Purple => new Color(128, 0, 128);
+
+    private int twoFiveFive(int x)
+    {
+        if (x < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{x} changed to lowest possible value: 0");
+            Console.ResetColor();
+            x = 0;
+        }
+        else if (x > 255)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{x} changed to highest possible value: 255");
+            Console.ResetColor();
+            x = 255;
+        }
+
+        return x;
+    }
+}
+
+/// <summary>
+/// Card Class that has Colors and Ranks
+/// </summary>
+internal class Card
+{
+    public CardColor Color { get; init; }
+    public CardRank Rank { get; init; }
+
+    internal Card()
+    {
+    }
+
+    internal Card(CardColor cardColor, CardRank cardRank)
+    {
+        Color = cardColor;
+        Rank = cardRank;
+    }
+
+    public bool RankChecker() => (int)Rank > 10;
+}
+
+/// <summary>
+/// 4 Color Cards R,G,B,Y
+/// </summary>
+public enum CardColor
+{
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
+
+/// <summary>
+/// 10 Number Cards and 4 Face Cards
+/// </summary>
+public enum CardRank
+{
+    One = 1,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Dollar,
+    Percentage,
+    Carat,
+    Ampersand
+}
+
