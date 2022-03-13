67391a0f34efc1b4ab5ba99ce9e1346fae258afd
rdthree <1165540+rdthree@users.noreply.github.com>
2022-03-12 21:07:03 -0500

    completed rock paper scissors game

--- Level24_Game01_RockPaperScissors/Program.cs
+++ Level24_Game01_RockPaperScissors/Program.cs
@@ -3,7 +3,9 @@
 //var dude = new Player();
 //dude.Choose();
 
-var go = new RockPaperScissorsGame();
+var go = new RockPaperScissorsGame(10);
+go.Play();
+
 
 // player one: choose
 // player two: choose
@@ -13,31 +15,116 @@ var go = new RockPaperScissorsGame();
 
 internal class RockPaperScissorsGame
 {
-    public int Round { get; private set; }
+    private int Round { get; set; }
+    private readonly int _maxRounds;
+    private readonly Player _playerOne;
+    private readonly Player _playerTwo;
 
-    internal RockPaperScissorsGame()
+    internal RockPaperScissorsGame(int maxRounds)
     {
-        Round = 0;
-        
+        _maxRounds = maxRounds;
         Console.WriteLine("Player 1");
-        var player1 = new Player();
+        _playerOne = new Player();
         Console.WriteLine("Player 2");
-        var player2 = new Player();
+        _playerTwo = new Player();
     }
 
-    WinLoseDraw Results()
+    public void Play()
     {
-        
-        return WinLoseDraw.Draw;
+        Console.WriteLine($"Best out of {_maxRounds} Rounds");
+        while (Round < 10)
+        {
+            PlayRound();
+            Stats();
+        }
+
+        FinalStats();
+    }
+
+    private void PlayRound()
+    {
+        _playerOne.Choose();
+        _playerTwo.Choose();
+
+        RoundResults();
+
+        Round++;
+    }
+
+    private void RoundResults()
+    {
+        if (_playerOne.Choice != _playerTwo.Choice)
+        {
+            if (_playerOne.Choice == RockPaperScissors.Rock & _playerTwo.Choice == RockPaperScissors.Scissors)
+                _playerOne.WinRound = true;
+            else if (_playerOne.Choice == RockPaperScissors.Scissors & _playerTwo.Choice == RockPaperScissors.Paper)
+                _playerOne.WinRound = true;
+            else if (_playerOne.Choice == RockPaperScissors.Paper & _playerTwo.Choice == RockPaperScissors.Rock)
+                _playerOne.WinRound = true;
+            else _playerTwo.WinRound = true;
+
+            WinLose();
+        }
+
+        if (_playerOne.Choice != _playerTwo.Choice) return;
+        _playerOne.Draws++;
+        _playerTwo.Draws++;
+    }
+
+    private void WinLose()
+    {
+        if (_playerOne.WinRound)
+        {
+            _playerOne.Wins++;
+            _playerTwo.WinRound = false;
+            _playerTwo.Losses++;
+        }
+        else if (_playerTwo.WinRound)
+        {
+            _playerTwo.Wins++;
+            _playerOne.WinRound = false;
+            _playerOne.Losses++;
+        }
+    }
+
+    private void FinalStats()
+    {
+        Console.WriteLine("__________________________");
+
+        if (_playerOne.Wins == _playerTwo.Wins)
+            Console.WriteLine("ITS A DRAW");
+        else if (_playerOne.Wins > _playerTwo.Wins)
+            Console.WriteLine($"{_playerOne.Name} WINS BY {_playerOne.Wins - _playerTwo.Wins} POINTS");
+        if (_playerOne.Wins < _playerTwo.Wins)
+            Console.WriteLine($"{_playerTwo.Name} WINS BY {_playerTwo.Wins - _playerOne.Wins} POINTS");
+    }
+
+    private void Stats()
+    {
+        Console.WriteLine($"Rounds: {Round}");
+        Console.WriteLine($"Draws: {_playerOne.Draws}");
+        Console.WriteLine("__________________________");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{_playerOne.Name}");
+        Console.WriteLine($"Wins: {_playerOne.Wins}");
+        Console.WriteLine($"Losses: {_playerOne.Losses}");
+        Console.ResetColor();
+        Console.WriteLine("__________________________");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{_playerTwo.Name}");
+        Console.WriteLine($"Wins: {_playerTwo.Wins}");
+        Console.WriteLine($"Losses: {_playerTwo.Losses}");
+        Console.ResetColor();
     }
 }
 
 internal class Player
 {
     public string? Name { get; init; }
-    public int Wins { get; private set; }
-    public int Losses { get; private set; }
-    public int Draws { get; private set; }
+    public int Wins { get; internal set; }
+    public bool WinRound { get; internal set; }
+    public int Losses { get; internal set; }
+    public int Draws { get; internal set; }
     public RockPaperScissors Choice { get; private set; }
 
     internal Player()
@@ -45,7 +132,7 @@ internal class Player
         Wins = 0;
         Losses = 0;
         Draws = 0;
-        
+
         Console.Write("input player name: ");
         Name = Console.ReadLine();
     }
@@ -53,20 +140,20 @@ internal class Player
     internal RockPaperScissors Choose()
     {
         Console.Write($"{Name}, make a choice: 'r', 'p', 's': ");
-        string input = Console.ReadLine();
+        string? input = Console.ReadLine();
         Choice = input switch
         {
             "r" => RockPaperScissors.Rock,
             "p" => RockPaperScissors.Paper,
-            "s" => RockPaperScissors.Scissors
+            "s" => RockPaperScissors.Scissors,
+            _ => throw new ArgumentOutOfRangeException()
         };
         Console.WriteLine($"you have chosen {Choice}");
         return Choice;
     }
-    
+
     internal class Win
     {
-        
     }
 }
 
