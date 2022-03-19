1d65a01217b4967037c3c56b1339787651035796
rdthree <1165540+rdthree@users.noreply.github.com>
2022-03-19 19:53:49 -0400

    win lose logic works, need some error checks for input and also running stats for players

--- Level24_Game04_TicTacToe/Program.cs
+++ Level24_Game04_TicTacToe/Program.cs
@@ -1,10 +1,13 @@
-﻿var game = new Game();
+﻿using Microsoft.VisualBasic;
+
+var game = new Game();
 game.Run();
 
 internal class Game
 {
     // ticTacToe
     private TicTacToe _ticTacToe;
+    private GameState _gameState;
     private Player _player1;
     private Player _player2;
 
@@ -16,27 +19,30 @@ internal class Game
         _ticTacToe = new TicTacToe();
         _player1 = new Player(XO.X);
         _player2 = new Player(XO.O);
+        _gameState = new GameState(_ticTacToe);
     }
 
     public void Run()
     {
-        //while (true)
-        //{
-        Console.WriteLine($"Player One, choose a tile:");
-        _ticTacToe.PlayerMoveIndex(_player1, _player1.Move, _player1.Mark);
+        while (true)
+        {
+            Console.WriteLine($"Player One, choose a tile:");
+            _ticTacToe.PlayerMoveIndex(_player1);
+            _ticTacToe.Board();
+            _gameState.WinConditions();
 
-        Console.WriteLine($"Player Two, choose a tile:");
-        _ticTacToe.PlayerMoveIndex(_player1, _player2.Move, _player2.Mark);
-        //run that game
-        _ticTacToe.Board();
-        //}
+            Console.WriteLine($"Player Two, choose a tile:");
+            _ticTacToe.PlayerMoveIndex(_player2);
+            _ticTacToe.Board();
+            _gameState.WinConditions();
+        }
     }
 }
 
 internal class TicTacToe
 {
     // game board
-    private readonly string[] Squares = new string[9];
+    public string[] Squares { get; private set; } = new string[9];
     // locations 1-9
     // compare current selected
     // best out of 5
@@ -69,20 +75,114 @@ internal class TicTacToe
         }
     }
 
-    internal void PlayerMoveIndex(Player player, int move, XO xo)
+    internal void PlayerMoveIndex(Player player)
     {
-        move = player.MakeMove();
-        if (xo == XO.X)
-            Squares[move] = "X";
-        if (xo == XO.O)
-            Squares[move] = "O";
+        var move = player.MakeMove();
+        if (Squares[move] != "-")
+        {
+            Console.WriteLine("that's already taken");
+            PlayerMoveIndex(player);
+        }
+
+        // a little safeguard to an incorrect move doesn't overwrite the previous move
+        if (Squares[move] == "-")
+        {
+            Squares[move] = player.Mark switch
+            {
+                XO.X => "X",
+                XO.O => "O",
+            };
+        }
     }
 }
 
 internal class GameState
 {
-    // moves remaining
+    private TicTacToe _ticTacToe;
+    public int XWin { get; private set; }
+    public int YWin { get; private set; }
+
+    internal GameState(TicTacToe ticTacToe)
+    {
+        _ticTacToe = ticTacToe;
+    }
+
+    internal bool WinConditions()
+    {
+        var xWins = "X wins";
+        var yWins = "X wins";
+        var vert = 0;
+        var horiz = 0;
+        var diag = 0;
+
+        // horiz
+        for (int i = 0; i < 7; i++)
+        {
+            if (0 % 3 == 0)
+            {
+                if (_ticTacToe.Squares[i] == _ticTacToe.Squares[i + 1] &&
+                    _ticTacToe.Squares[i + 1] == _ticTacToe.Squares[i + 2])
+                {
+                    whoWon(i);
+                    //Console.WriteLine($"{_ticTacToe.Squares[i]}, {_ticTacToe.Squares[i+1]}, {_ticTacToe.Squares[i+2]}");
+                    return true;
+                }
+            }
+        }
+
+        //vert
+        for (int i = 0; i < 3; i++)
+        {
+            if (_ticTacToe.Squares[i] == _ticTacToe.Squares[i + 3] &&
+                _ticTacToe.Squares[i + 3] == _ticTacToe.Squares[i + 6])
+            {
+                whoWon(i);
+                return true;
+            }
+        }
+
+        //diag
+        for (int i = 0; i < 3; i++)
+        {
+            if (i == 0)
+            {
+                if (_ticTacToe.Squares[i] == _ticTacToe.Squares[i + 4] &&
+                    _ticTacToe.Squares[i + 4] == _ticTacToe.Squares[i + 8])
+                {
+                    whoWon(i);
+                    return true;
+                }
+            }
+
+            if (i == 2)
+            {
+                if (_ticTacToe.Squares[i] == _ticTacToe.Squares[i + 2] &&
+                    _ticTacToe.Squares[i + 2] == _ticTacToe.Squares[i + 4])
+                {
+                    whoWon(i);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
     // win / lose / cat
+
+    private void whoWon(int i)
+    {
+        if (_ticTacToe.Squares[i] == "-") return;
+        if (_ticTacToe.Squares[i] == "X")
+        {
+            Console.WriteLine("X Wins");
+            XWin++;
+        }
+        else
+        {
+            Console.WriteLine("Y Wins");
+            YWin++;
+        }
+    }
 }
 
 internal class Player
