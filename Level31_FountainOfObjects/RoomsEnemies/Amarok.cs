60fdb6312ba6fbe59c738ead99dd07be3a14f735
rdthree <1165540+rdthree@users.noreply.github.com>
2022-03-30 21:55:34 -0400

    rooms

---
+++ Level31_FountainOfObjects/RoomsEnemies/Amarok.cs
@@ -0,0 +1,33 @@
+﻿namespace Level31_FountainOfObjects.RoomsEnemies;
+
+internal class Amarok : MainRoom, ISubRoom
+{
+    public Amarok(int rows, int columns) : base(rows, columns)
+    {
+        AmarokLocation = new IMainRoom.Coordinate(Rows - 12, Columns - 46);
+        LocateSenses();
+    }
+
+    internal IMainRoom.Coordinate AmarokLocation { get; }
+    internal List<IMainRoom.Coordinate> AmarokCoords { get; } = new();
+    internal List<IMainRoom.Coordinate> AmarokSmellCoords { get; } = new();
+
+    protected override void ItemSenseCoordinates(int i, int j)
+    {
+        SenseCoordinateAdjacent(i, j, AmarokLocation, AmarokCoords);
+    }
+
+    protected override void AllSenseCoordinates(int i, int j)
+    {
+        SenseCoordinate(i, j, AmarokLocation, 1, 1, AmarokSmellCoords);
+    }
+
+    protected override SenseTypesCoordinates SenseTypeSelector(List<IMainRoom.Coordinate> sense)
+    {
+        SenseTypesCoordinates senseTypeCoordinates;
+        if (sense == AmarokCoords) senseTypeCoordinates = SenseTypesCoordinates.Death;
+        else if (sense == AmarokSmellCoords) senseTypeCoordinates = SenseTypesCoordinates.Fear;
+        else senseTypeCoordinates = SenseTypesCoordinates.Nothing;
+        return senseTypeCoordinates;
+    }
+}
