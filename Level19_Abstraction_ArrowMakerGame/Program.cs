eb7bc8b602cd839d97111bcd5173924d5c60fa84
rdthree <1165540+rdthree@users.noreply.github.com>
2022-03-31 20:20:38 -0400

    refactored and suppressed some resharper messages

--- Level19_Abstraction_ArrowMakerGame/Program.cs
+++ Level19_Abstraction_ArrowMakerGame/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using MonchoUtils;
+// ReSharper disable RedundantAssignment
 
 // rectangle class with getter and setter for width/height
 // methods for area
