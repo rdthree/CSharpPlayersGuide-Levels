1961907a69a4e316c6aa725c753d036758b62a90
rdthree <1165540+rdthree@users.noreply.github.com>
2022-04-08 20:58:07 -0400

    added a TODO for use of the map

--- Level31_FountainOfObjects/Program.cs
+++ Level31_FountainOfObjects/Program.cs
@@ -2,6 +2,7 @@
 // TODO: fix movement of players, SubRooms so they never leave game board, use a wraparound technique
 // TODO: improve text messages for map-less gameplay
 // TODO: help (h) feature to give idea of gameplay and messages
+// TODO: using the map takes 25 moves, add a message to indicate this
 
 //var game = new Game(40, 60);
 var game = new Game();
