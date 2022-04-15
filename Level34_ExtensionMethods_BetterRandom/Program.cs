13a802752e064b0e311326250d674d30f0a271e0
rdthree <1165540+rdthree@users.noreply.github.com>
2022-04-14 21:28:14 -0400

    simplified extension methods

--- Level34_ExtensionMethods_BetterRandom/Program.cs
+++ Level34_ExtensionMethods_BetterRandom/Program.cs
@@ -8,18 +8,22 @@
 
 double nextTen;
 
-//var loop = new Random().NextDouble().NextMaxDouble(9);
-var loop = new Random();
-loop.NextMaxDouble(5.0f);
-var poop = new Random().NextMaxDouble(9.0);
+var loop = new Random().NextMaxDouble(9.0);
+var stringers = new string[] {"yo", "so", "flow", "crow", "man", "barn"};
+
+
 
 for (var i = 0; i < 10; i++)
 {
     nextTen = new Random().NextDouble() * 10.0;
     var nextWord = new Random().Next(0, 4);
     
-    Console.WriteLine($"next double: {nextTen}");
-    Console.WriteLine($"next word {words[nextWord]}");
+    //Console.WriteLine($"next double: {nextTen}");
+    //Console.WriteLine($"next word {words[nextWord]}");
+    
+    var stingO = new Random().NextString(stringers);
+    //Console.WriteLine(stingO);
+    
 }
 
 for (var i = 0; i < 10; i++)
@@ -31,20 +35,19 @@ for (var i = 0; i < 10; i++)
     {
         var fairOdds = new Random().Next(0, 1);
         coinFlip = fairOdds == 0;
-        Console.WriteLine($"fair coin toss, (true is heads, false is tails) {coinFlip}");
+        //Console.WriteLine($"fair coin toss, (true is heads, false is tails) {coinFlip}");
     }
     else if (nextTen < 5)
     {
         var unfairOdds = new Random().NextDouble() * 1.5;
         coinFlip = unfairOdds > 0.5;
-        Console.WriteLine($"unfair coin toss, (true is heads, false is tails) {coinFlip}");
+        //Console.WriteLine($"unfair coin toss, (true is heads, false is tails) {coinFlip}");
     }
 }
 
 public static class RandomExtensions
 {
-    public static double NextMaxDouble(this Random rand, double input)
-    {
-        return rand.NextDouble() * input;
-    }
+    public static double NextMaxDouble(this Random rand, double input) => rand.NextDouble() * input;
+    public static string NextString(this Random rand, params string[] strings) => strings[rand.Next(strings.Length)];
+    public static bool CoinFlip(this Random rand, double odds = 0.5) => rand.NextDouble() > odds;
 }
