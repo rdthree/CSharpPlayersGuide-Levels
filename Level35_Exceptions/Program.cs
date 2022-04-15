ba75c6d489d15de3805b9366133a882694bd6ed0
rdthree <1165540+rdthree@users.noreply.github.com>
2022-04-15 08:48:11 -0400

    added sample programs and updated exceptions chapter

---
+++ Level35_Exceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// 2 players
+// pick random numbers
+// if random number previously chosen then flag it
+// if random number was secretly chosen by the game - game over, throw an exception
+// use try/catch block to handle exception and display results
+
+var rand = new Random();
+
+var boom = rand.Next(10);
+var guessed = new List<int>();
+Console.WriteLine("guess a number 1 to 10");
+
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("guess");
+    int.TryParse(Console.ReadLine(), out int guess);
+    guessed.Add(guess);
+}
